005108fcf88ccaa6f3875fe8461c5e6ded6209c2
python11021 <khhpython1121@gmail.com>
2025-09-23 17:21:05 +0900

    db 조회 파일 분리, 지역별 등록 현황 추가

--- elec_by_year.py
+++ elec_by_year.py
@@ -4,56 +4,16 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-# DB 관련 import
-import pymysql
-import os
-from dotenv import load_dotenv
-
-# 환경변수 호출.
-load_dotenv()
-
-
-DB_NAME = "6team" 
-
-# db 연결
-# 1. .env 파일 데이타 불러오기 & DB 연결 
-def get_connection():
-    return pymysql.connect(
-        host=os.getenv("DB_HOST")
-        ,user=os.getenv("DB_USER")
-        ,password=os.getenv("DB_PASSWORD")
-        ,charset='utf8mb4'       # MySQL, MariaDB 데이터베이스(DB)에 연결할 때, 문자의 인코딩 방식을 지정하는 옵션
-        ,database=DB_NAME
-    )
-
-# EV_REGISTRATION 테이블 연도별 전기차 현황 조회
-def elec_year_list(isDict = False):
-    with get_connection() as conn:
-        sql = '''
-                SELECT YEAR
-                     ,SUM(COUNT) AS SUM_YEAR
-                  FROM EV_REGISTRATION
-                GROUP BY YEAR 
-              '''   
-        x = []
-        y = []
-        with conn.cursor() as cur:
-            cur.execute(sql)
-            for c in cur.fetchall():            
-                x.append(c[0])
-                y.append(int(c[1]))
-        return x,y
+import elecdb   # 6team 디비 
 
 # EV_REGISTRATION 테이블 연도별 전기차 현황 데이타 datas에 저장
-datas =elec_year_list()    
+datas =elecdb.elec_yearstatus_list()    
 
 # x축: 연도, y축: 등록대수
 df = pd.DataFrame({
     '연도': datas[0],
     '등록대수': datas[1]
 })
-
-print(datas)
 
 x = datas[0]    # 연도
 y = datas[1]    # 등록대수
